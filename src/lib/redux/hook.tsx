713e8efe96acfd074ff2e9623d97e4372d150a4d
nicweke <nicweke@gmail.com>
2024-07-13 12:44:24 +0300

    added hooks and deep merge

--- src/lib/redux/hook.tsx
+++ src/lib/redux/hook.tsx
@@ -1,10 +1,21 @@
-import { useEffect } from "react"
-import { AppDispatch, RootState, store } from "@/lib/redux/store"
-import { saveStateToLocalStorage } from "@/lib/redux/local-storage"
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react";
+import { AppDispatch, RootState, store } from "@/lib/redux/store";
+import {
+  loadStateFromLocalStorage,
+  saveStateToLocalStorage,
+} from "@/lib/redux/local-storage";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { deepMerge } from "@/lib/deep-merge";
+import { initialResumeState, setResume } from "@/lib/redux/resumeSlice";
+import { Resume } from "@/lib/redux/types";
+import {
+  Settings,
+  initialSettings,
+  setSettings,
+} from "@/lib/redux/settingsSlice";
 
-export const useAppDispatch : () => AppDispatch = useDispatch
-export const useAppSelector : TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const useSaveStateToLocalStorageOnChange = () => {
   useEffect(() => {
@@ -12,11 +23,27 @@ export const useSaveStateToLocalStorageOnChange = () => {
       saveStateToLocalStorage(store.getState());
     });
     return unsubscribe;
-  }, [])
-      
-}
+  }, []);
+};
 
 export const useSetInitialStore = () => {
-    const dispatch = 
-
-}
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    const state = loadStateFromLocalStorage();
+    if (!state) return;
+    if (state.resume) {
+      const mergedResumeState = deepMerge(
+        initialResumeState,
+        state.resume
+      ) as Resume;
+      dispatch(setResume(mergedResumeState));
+    }
+    if (state.settings) {
+      const mergedSettingsState = deepMerge(
+        initialSettings,
+        state.settings
+      ) as Settings;
+      dispatch(setSettings(mergedSettingsState));
+    }
+  }, []);
+};
